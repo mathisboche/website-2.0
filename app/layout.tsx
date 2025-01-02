52cd39c472c495cc8cb5f3c0c8d95081b91e6a82
Mathis Boche <mathisboche@outlook.fr>
2025-01-02 07:17:48 +0100

    suppressions des variables inutiles const geistSans et const geistMono

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,15 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from 'next/script'; // Import pour Google Analytics
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+//const geistSans = Geist({
+//  variable: "--font-geist-sans",
+//  subsets: ["latin"],
+//});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+//const geistMono = Geist_Mono({
+//  variable: "--font-geist-mono",
+//  subsets: ["latin"],
+//});
 
 // Remplace par TON ID de suivi Google Analytics
 const GA_TRACKING_ID = 'G-9KCS6YWF1D';
